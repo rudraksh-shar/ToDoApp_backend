b7723d0bddbe5c44c0642ffee56f3f64cebef28d
RUDRAKSH SHARMA <rudrakshsharma@RUDRAKSHs-MacBook-Pro.local>
2024-10-28 14:25:00 +0530

    final change

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,23 @@
+//src/main.ts
+/*import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.enableCors(); // Enable CORS
+  await app.listen(3000);
+}
+bootstrap();*/
 // src/main.ts
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: 'https://your-frontend-url.vercel.app', // Replace with your frontend URL
-  });
-  await app.listen(3000);
+    const app = await NestFactory.create(AppModule);
+    app.enableCors({
+        origin: 'https://to-do-app-frontend-xi.vercel.app/', // Replace with your frontend URL
+    });
+    await app.listen(3000);
 }
 bootstrap();
+
