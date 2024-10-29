09f930fa01605a5d58a14c3f315e151f7b48c4d0
RUDRAKSH SHARMA <rudrakshsharma@RUDRAKSHs-MacBook-Pro.local>
2024-10-29 09:53:40 +0530

    Cors has been changed

--- src/main.ts
+++ src/main.ts
@@ -15,7 +15,7 @@ import { NestFactory } from '@nestjs/core';
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     app.enableCors({
-        origin: 'https://to-do-app-frontend-xi.vercel.app/', 
+        origin: "*", 
     });
     await app.listen(3000);
 }
